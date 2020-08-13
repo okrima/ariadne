e25f7683816a906eeb52e1713907855c378d4edb
Mirko Albanese <mirko.albanese.94@gmail.com>
2020-08-13 11:09:55 +0200

    Add files via upload

--- source/dynamics/1D_pde.cpp
+++ source/dynamics/1D_pde.cpp
@@ -37,18 +37,13 @@ namespace Ariadne{
         return uts;
     }
 
-    //Formattare la soluzione in modo da avere un array formato da tante tuple di vettori, in cui una
-    //tupla sia la soluzione al primo istante di tempo, la seconda la soluzione al secondo istante 
-    //di tempo e così via. Usare Tensor<2, Array<double> per definire tempo e spazio e usare un Array<double>
-    //per definire il vettore nello spazio.
-
     //Solving one dimensional pde
-    Tensor<2, FloatMP> pde_1Dsolver(std::function<FloatMP(FloatMP)>& phi0, string1D& stringParameter, SizeType Nx)
+    Tensor<2, FloatMP> pde_1Dsolver(std::function<FloatMP(FloatMP)>& phi0, std::function<FloatMP(FloatMP, FloatMP)>& source,string1D& stringParameter, SizeType Nx)
     {
-        FloatMP c = sqrt((stringParameter.tension/stringParameter.massPerUnit));
+        FloatMP c = sqrt((stringParameter.tension/(stringParameter.mass/stringParameter.length)));
         //Real c = stringParameter.frequency*stringParameter.wavelength;
         //Real c = 342;
-        FloatMP T = 0.5;
+        FloatMP T = 2.0;
 
         FloatMP C2 = pow(stringParameter.CourantNumber, 2);
 
@@ -59,41 +54,28 @@ namespace Ariadne{
         FloatMP Nt = round(T/dt);
         SizeType Ntime = Nt.get_d();
 
+        Array<FloatMP> time = linspace(T, Ntime);
+
         Tensor<2, FloatMP> uts({Nx, Ntime}, 0);
 
         //Array<Real> time = linspace(Nt*dt, Nt);
         //DoublePrecision pr;
         FloatMP k = 2*pi_opp()/stringParameter.wavelength;
 
+        std::cout << "Computing";
+
         uts = setIC(uts, phi0, Nx, space);
 
-        // Set Boundary Condition
-        //uts[{0, 0}] = 0;
-        //uts[{Nx, 0}] = 0;
-        //solution.nextV[0] = 0;
-        //solution.nextV[Nx.get_d() - 1] = 0;
-
+        std::cout << " .";
         // Set first Time step
         SizeType n = 0;
         for (SizeType i = 1; i < Nx - 1; i++)
         {
-            uts[{i, n+1}] = uts[{i, n}] - 0.5*C2*(uts[{i-1, n}] - 2*uts[{i, n}] + uts[{i+1, n}]);
-/*
-            solution.nextV[i] = solution.currV[i] //+ dt*velIC
-                            - 0.5_q*C2*(solution.currV[i-1] - 2*solution.currV[i] + solution.currV[i+1]);
-                            // + 0.5_q*pow(dt,2)*source(space[i], time[n])
-*/
-        }//first time step
+            uts[{i, n+1}] = uts[{i, n}] - 0.5*C2*(uts[{i-1, n}] - 2*uts[{i, n}] + uts[{i+1, n}])
+                + pow(dt,2)*source(space[i], time[n]);
+        }// first time step
 
-/*
-        solution.nextV[0] = 0;
-        solution.nextV[Nx.get_d() - 1] = 0;
-        
-        //switch variables
-
-        solution.prevV = solution.currV;
-        solution.currV = solution.nextV;
-*/
+        std::cout << " .";
         // For each time step - main loop
         for (n = 1; n < Nt.get_d(); n++)
         {
@@ -101,23 +83,10 @@ namespace Ariadne{
             for (SizeType i = 1; i < Nx - 1; i++)
             {
                 uts[{i, n+1}] = (1/(1+0.5*stringParameter.damping*dt))*(2*uts[{i, n}] - uts[{i, n-1}] + (stringParameter.damping*dt/2)*uts[{i, n-1}] +
-                    C2*(uts[{i-1, n}] - 2*uts[{i, n}] + uts[{i+1, n}]));
-/*
-                solution.nextV[i] = (1/(1+0.5_q*stringParameter.damping*dt))*(-(solution.prevV[i])+1.1_q*solution.currV[i] + (stringParameter.damping*dt/2)*solution.prevV[i] +
-                    C2*(solution.currV[i-1] - 2*solution.currV[i] + solution.currV[i+1]));
-                    //+ pow(dt,2)*source(space[i], time[n]))
-*/          
+                    C2*(uts[{i-1, n}] - 2*uts[{i, n}] + uts[{i+1, n}])) + 1000*pow(dt, 2)*source(space[i], time[n]);        
             }
-/*
-            solution.nextV[0] = 0;
-            solution.nextV[Nx.get_d() - 1] = 0;
-
-            //switch variables
-            solution.prevV = solution.currV;
-            solution.currV = solution.nextV;
-*/
         }//main loop
-
+        std::cout << " .";
         return uts;
     }
         
