9af181fe491a449e4d7c6640f15ff3a251a82586
Mirko Albanese <mirko.albanese.94@gmail.com>
2020-09-08 15:23:07 +0200

    Add files via upload

--- examples/pdes/acoustic_1D_pde.cpp
+++ examples/pdes/acoustic_1D_pde.cpp
@@ -1,23 +1,20 @@
 #include "numeric/numeric.hpp"
-#include "numeric/real.hpp"
-#include "algebra/algebra.hpp"
 #include "algebra/vector.hpp"
 #include "utility/array.hpp"
 
 #include "output/gnuplot.hpp"
 #include "dynamics/1D_pde.hpp"
 
-#include <iostream>
-#include <fstream>
-#include <cstdlib>
-#include <unistd.h>
+#include "utility/gnuplot-iostream.h"
 
 using namespace Ariadne;
 using namespace std;
 
 int main() {
 
-    SizeType Nx = 10;                // # point in space
+    SizeType Nx = 10;             // # point in space
+
+    pde1D solver1D = pde1D();
 
     //pde_solution_1D solution;
     Parameter1D stringModel;
@@ -25,55 +22,57 @@ int main() {
     stringModel.length = 1.0;  // Length
     stringModel.tension = 100000;
     stringModel.mass = 4;
-    FloatMP x0 = 0.85*stringModel.length;  // Point of max amplitube - Triangular IC
-    stringModel.wavelength = 2*stringModel.length;
+    FloatDP x0 = 0.85*stringModel.length;  // Point of max amplitube - Triangular IC
+    FloatDP wavelength = 2*stringModel.length;
     //Real c = string.frequency*string.wavelength;
-    FloatMP amp = 0.8;
+    FloatDP amp = 0.8;
     stringModel.damping = 100;
 
-    stringModel.c = sqrt((stringModel.tension/(stringModel.mass/stringModel.length)));
+    FloatDP c = sqrt((stringModel.tension/(stringModel.mass/stringModel.length)));
 
-    stringModel.frequency = stringModel.c/stringModel.wavelength; // Frequency
+    FloatDP frequency = c/wavelength; // Frequency
 
     //Real T = string.wavelength/c;
     stringModel.CourantNumber = 0.8;
     //Real C2 = pow(string.CourantNumber, 2);
 
-    FloatMP k = 2*pi_opp()/stringModel.wavelength;
-    FloatMP omega = 2*pi_opp()*stringModel.frequency;
+    FloatDP k = 2*pi_opp()/wavelength;
+    FloatDP omega = 2*pi_opp()*frequency;
 
     // Set function initial position condition
-    //std::function<FloatMP(FloatMP)> PosInitSteady = [&](FloatMP x){return sin(k*x);};  //Steady-state wave
+    //std::function<FloatDP(FloatDP)> PosInitSteady = [&](FloatDP x){return sin(k*x);};  //Steady-state wave
    
-    std::function<FloatMP(FloatMP)> PosInitTri = [&](FloatMP x){ //Triangular
+    std::function<FloatDP(FloatDP)> PosInitTri = [&](FloatDP x){ //Triangular
         if (x.get_d() < x0.get_d())
             return amp*(x/x0);
         else
             return amp*(stringModel.length - x)/(stringModel.length - x0);
     };
 
-    std::function<FloatMP(FloatMP, FloatMP)> source = [&](FloatMP x, FloatMP t){return (sin(k*x)*cos(omega*t));};
+    std::function<FloatDP(FloatDP, FloatDP)> source = [&](FloatDP x, FloatDP t){return (sin(k*x)*cos(omega*t));};
 
-    auto data = pde_1Dsolver(PosInitTri, source, stringModel, Nx+1);
+    auto data = solver1D.pde_1Dsolver(PosInitTri, source, stringModel, Nx+1);
   
 
     //auto VelInitCond = [&](Real x){return 0;};            // Set function initial velocity position
    
-    Gnuplot gp;
+    Gnuplot gp = Gnuplot("tee plot.gnu | gnuplot -persist");
+
     GnuplotCanvas canvas = GnuplotCanvas();
 
     Image2D image;
+    _Range2D range;
 
-    canvas.setTerminal(gp, image, _png, "string-SpaceEvol");
-    canvas.setMultiplot(gp, true);
+    canvas.setTerminal(gp, _gif, "string-TimeEvol");
+    canvas.setMultiplot(gp, false);
     canvas.setTitle(gp, "String Evolution with triangular IC");
     canvas.setXLabel(gp, "x - Space");
     canvas.setYLabel(gp, "Amplitude");
-    canvas.setRange2D(image, 0, Nx+1, -1, 1);
+    canvas.setRange2D(range, 0, Nx+1, -1, 1);
 
-    canvas.plotTensor2D(gp, image, data);
+    //Array<FloatDP> space = linspace(stringModel.length, Nx+1);
 
+    canvas.plotTensor2D(gp, image, range, data);
 
     return 0;
-
 }
--- examples/pdes/acoustic_2D_pde.cpp
+++ examples/pdes/acoustic_2D_pde.cpp
@@ -1,16 +1,12 @@
 #include "numeric/numeric.hpp"
-#include "numeric/real.hpp"
-#include "algebra/algebra.hpp"
+
 #include "algebra/vector.hpp"
 #include "utility/array.hpp"
 
 #include "output/gnuplot.hpp"
 #include "dynamics/2D_pde.hpp"
 
-#include <iostream>
-#include <fstream>
-#include <cstdlib>
-#include <unistd.h>
+#include "utility/gnuplot-iostream.h"
 
 using namespace Ariadne;
 using namespace std;
@@ -20,37 +16,40 @@ int main(){
     SizeType Nx = 10;   //Mesh 1° dim
     SizeType Ny = 10;   //Mesh 2° dim
 
+    pde2D solver2D = pde2D();
+
     Parameter2D firstDim, secondDim;
 
     firstDim.length = 10;
     secondDim.length = 10;
 
-    std::function<FloatMP(FloatMP,FloatMP)> IC = [&](FloatMP x, FloatMP y){return sin(pi_opp()*2*x/firstDim.length)*sin(pi_opp()*3*y/secondDim.length);};
-    std::function<FloatMP(FloatMP,FloatMP)> IC_Gauss = [&](FloatMP x, FloatMP y){return (exp(-0.5*pow(x-firstDim.length/2, 2)-0.5*pow(y-secondDim.length/2, 2)));};
+    std::function<FloatDP(FloatDP,FloatDP)> IC = [&](FloatDP x, FloatDP y){return sin(pi_opp()*2*x/firstDim.length)*sin(pi_opp()*3*y/secondDim.length);};
+    std::function<FloatDP(FloatDP,FloatDP)> IC_Gauss = [&](FloatDP x, FloatDP y){return (exp(-0.5*pow(x-firstDim.length/2, 2)-0.5*pow(y-secondDim.length/2, 2)));};
 
-    std::function<FloatMP(FloatMP, FloatMP, FloatMP)> source = [&](FloatMP x, FloatMP y, FloatMP t){return 0;};
+    std::function<FloatDP(FloatDP, FloatDP, FloatDP)> source = [&](FloatDP x, FloatDP y, FloatDP t){return 0;};
 
-    auto data = pde_2Dsolver(IC_Gauss, source, firstDim, secondDim, Nx+1, Ny+1);
+    auto data = solver2D.pde_2Dsolver(IC_Gauss, source, firstDim, secondDim, Nx+1, Ny+1);
 
     Gnuplot gp;
     GnuplotCanvas canvas;
 
     Image3D image;
+    _Range3D range3D;
     _Line3D line3D;
     line3D.style = surface3D;
 
-    canvas.setTerminal(gp, image, _gif, "TimeEvol");
+    canvas.setTerminal(gp, _gif, "TimeEvol3D");
     canvas.setMultiplot(gp, false);
     canvas.setTitle(gp, "Evolution");
     canvas.setXLabel(gp, "x - Space");
     canvas.setYLabel(gp, "y - Space");
     canvas.setZLabel(gp, "Amplitude");
-    canvas.setRange3D(image, Nx, Ny, 1);
+    canvas.setRange3D(range3D, Nx, Ny, 1);
     canvas.setLineStyle(image, line3D);
-    canvas.set3DPalette(gp, image, -1, 1, true);
+    canvas.set3DPalette(gp, image, -1, 1, 0.2, true);
     //canvas.setMap(gp);
 
-    canvas.plotTensor3D(gp, image, data);
+    canvas.plotTensor3D(gp, image, range3D, data);
 
     return 0;
 }
