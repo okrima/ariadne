f573efa465ae093ce3e67ef84ce1815b2f522d38
Mirko Albanese <mirko.albanese.94@gmail.com>
2020-11-30 18:37:59 +0100

    Modified figure::write method

--- python/source/graphics_submodule.cpp
+++ python/source/graphics_submodule.cpp
@@ -59,8 +59,10 @@ Void export_figure(pybind11::module& module)
     figure_class.def("draw",(Figure&(Figure::*)(const RealBox&))&Figure::draw, reference_internal);
     figure_class.def("draw",(Figure&(Figure::*)(const ApproximateBoxType&))&Figure::draw, reference_internal);
     figure_class.def("clear",&Figure::clear, reference_internal);
-    figure_class.def("write",(Void(Figure::*)(const Char*, bool)const)&Figure::write);
-    figure_class.def("write",(Void(Figure::*)(const Char*,Nat,Nat, bool)const)&Figure::write);
+    figure_class.def("write",(Void(Figure::*)(const Char*, CairoFileType)const)&Figure::write);
+    figure_class.def("write",(Void(Figure::*)(const Char*, GnuplotFileType)const)&Figure::write);
+    figure_class.def("write",(Void(Figure::*)(const Char*,Nat,Nat, CairoFileType)const)&Figure::write);
+    figure_class.def("write",(Void(Figure::*)(const Char*,Nat,Nat, GnuplotFileType)const)&Figure::write);
 }
 
 
